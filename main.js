80825e969c42b4a844036a79c76570b30776b71e
Martin Manerov <manerov97@yandex.ru>
2024-10-16 01:34:11 +0300

    added tooltips to points

--- main.js
+++ main.js
@@ -175,11 +175,38 @@ for (let lyrId in cfg.layers) {
         },
         //Настраиваем интерактивность
         onEachFeature: (feature, layer) => {
-            if (cfg.layers[lyrId].tooltip) {
+            // Надпись МО
+            if (cfg.layers[lyrId].label) {
                 layer
                     .bindTooltip(feature.properties.name, {
                         direction: "center",
                         className: "bounds-tooltip",
+                    })
+                    .openTooltip();
+            }
+            // Инфа о лице
+            if (cfg.layers[lyrId].tooltip) {
+                let toolTipContent = `
+                <table class="table table-hover">
+                    <tbody>
+                        <tr>
+                            <th scope="row">Наименование</th>
+                            <td>${feature.properties["Наименование"]}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">ОКВЭД</th>
+                            <td>${feature.properties["Категория"]} ${feature.properties["Название категории"]}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">ИНН</th>
+                            <td>${feature.properties["ИНН"]}</td>
+                        </tr>
+                    </tbody>
+                    </table>`;
+                layer
+                    .bindTooltip(toolTipContent, {
+                        interactive: true,
+                        className: "entity-tooltip",
                     })
                     .openTooltip();
             }
@@ -189,11 +216,6 @@ for (let lyrId in cfg.layers) {
                 },
                 mouseout: (e) => {
                     dehighlight(e.target, lyrId);
-                },
-                click: (e) => {
-                    if (cfg.layers[lyrId].infobox) {
-                        select(e.target, lyrId);
-                    }
                 },
             });
         },
@@ -210,147 +232,6 @@ for (let lyrId in cfg.layers) {
                 markersClusters[lyrId].addTo(map);
             }
             geoData[lyrId].addTo(map);
-        }
-    });
-
-    //Навешиваем интерактивность на объекты: всплывающее окно
-    geoData[lyrId].on("click", (e) => {
-        if (cfg.layers[lyrId].infobox) {
-            //Путь к папке с изображениями для объекта
-            let imagesFolder = `sourcedata/featuredata/layers/${lyrId}/${e.layer.feature.properties.id}/`;
-            //Массив источников фотографий
-            let imagesSources = [];
-            //Получаем данные из папки
-            $.ajax({
-                url: imagesFolder,
-                success: function (res) {
-                    let data = $(res).find("a");
-                    for (let i = 0; i < data.length; i++) {
-                        let dataHref = data[i].getAttribute("href");
-                        //Если запись в данных - название файла картинки, то записываем в массив
-                        if (dataHref.match(/\.(jpe?g|png|gif)$/)) {
-                            imagesSources.push(dataHref);
-                            //document.getElementsByClassName('v-infobox-image')[0].src = imgSrc;
-                        }
-                    }
-                },
-                async: false,
-            });
-
-            let slidesContainer =
-                document.getElementsByClassName("carousel-inner")[0];
-            let slides =
-                slidesContainer.getElementsByClassName("carousel-item");
-            let images = document.querySelectorAll(".carousel-item > img");
-            let carouselIndicators = document.getElementsByClassName(
-                "carousel-indicators"
-            )[0];
-            let carouselControls = document.querySelectorAll(
-                "#carouselIndicators > button"
-            );
-            //Очищаем галерею, кроме первого слайда
-            for (let i = 0; i < slides.length; i++) {
-                if (i == 0) slides[i].classList.add("active");
-                else slides[i].remove();
-            }
-            //Удаляем индикаторы
-            carouselIndicators.innerHTML = null;
-            // Если массив не пустой, то генерируем DOM слайдшоу
-            if (imagesSources.length > 0) {
-                imagesSources.forEach((value, index, array) => {
-                    //Делаем картинки
-                    if (index == 0) {
-                        images[0].src = value;
-                    } else {
-                        let slide = document.createElement("div");
-                        slide.classList.add("carousel-item");
-                        let image = document.createElement("img");
-                        image.src = value;
-                        image.classList.add("d-block");
-                        slide.appendChild(image);
-                        slidesContainer.appendChild(slide);
-                    }
-                    //Если картинка одна, то убираем управление
-                    if (imagesSources.length == 1) {
-                        carouselControls.forEach((btn) => {
-                            btn.classList.add("d-none");
-                        });
-                    }
-                    //Если картинок больше, чем 1, то добавляем управление
-                    else if (imagesSources.length > 1) {
-                        //Делаем кнопки для слайдшоу снизу
-                        let indicator = document.createElement("button");
-                        indicator.setAttribute("type", "button");
-                        indicator.setAttribute(
-                            "data-bs-target",
-                            "#carouselIndicators"
-                        );
-                        indicator.setAttribute(
-                            "data-bs-slide-to",
-                            String(index)
-                        );
-                        indicator.setAttribute(
-                            "aria-label",
-                            "Слайд " + String(index + 1)
-                        );
-                        if (index == 0) {
-                            indicator.classList.add("active");
-                            indicator.setAttribute("aria-current", "true");
-                        }
-                        carouselIndicators.appendChild(indicator);
-                        //Убираем display: none для всех кнопок
-                        carouselControls.forEach((btn) => {
-                            btn.classList.remove("d-none");
-                        });
-                    }
-                });
-            }
-            // Если массив пустой, то вставляем картинку "нет фото" и убираем управление слайдшоу
-            else {
-                images[0].src = "sourcedata/noimage.jpg";
-                carouselControls.forEach((value) => {
-                    value.classList.add("d-none");
-                });
-            }
-            //Путь к логотипу, который по умолчанию при отсутствии фото
-            //let pathToLogo = 'sourcedata/logos/' + cfg.layers[lyrId].logo;
-            //document.getElementsByClassName('v-infobox-image')[0].src = pathToLogo;
-            //Картинка "нет фото" при отсутствии фото
-            //document.getElementsByClassName('v-infobox-image')[0].setAttribute("onerror", "this.src='sourcedata/noimage.jpg'")
-            //document.getElementsByClassName('v-infobox-image')[0].src = 'sourcedata/streets/' + e.layer.feature.properties.id + '/header.jpg';
-            //Заголовок и описание во всплывающем окнк
-            let type_status_txt = "";
-            if (e.layer.feature.properties.type != null) {
-                type_status_txt = `${e.layer.feature.properties.type}. `;
-            }
-            if (e.layer.feature.properties.status != null) {
-                type_status_txt += `${e.layer.feature.properties.status}. `;
-            }
-            //type_status_txt = `${e.layer.feature.properties.type}. ${e.layer.feature.properties.status}.`
-            if (e.layer.feature.properties.title != null) {
-                document.getElementById("v-infobox-title").innerHTML =
-                    e.layer.feature.properties.title;
-                //document.getElementById('v-infobox-text').innerHTML = fillInfoBoxDescription(e.layer.feature.properties.id).splitTxtDesc()
-                document.getElementById("v-infobox-text").innerHTML =
-                    type_status_txt;
-            } else {
-                document.getElementById("v-infobox-title").innerHTML =
-                    type_status_txt;
-                document.getElementById("v-infobox-text").innerHTML = "";
-            }
-            if (e.layer.feature.properties.description) {
-                document.getElementById("v-infobox-text").innerHTML =
-                    "<p>" +
-                    e.layer.feature.properties.description.replace(
-                        /\n/g,
-                        "<p></p>"
-                    ) +
-                    "</p>";
-            }
-            document
-                .getElementsByClassName("v-infobox")[0]
-                .classList.add("v-infobox-open");
-            document.getElementsByClassName("v-infobox")[0].scrollTop = 0;
         }
     });
 }
