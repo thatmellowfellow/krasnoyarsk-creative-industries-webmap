53c8330569455c304c86aca120ff0156d9807e53
Martin Manerov <manerov97@yandex.ru>
2024-10-17 11:56:47 +0300

    updated config and packages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,29 @@
 import { defineConfig } from "vite";
 import { ViteMinifyPlugin } from "vite-plugin-minify";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [ViteMinifyPlugin()],
+    plugins: [
+        ViteMinifyPlugin(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: "js",
+                    dest: "", //means public folder
+                },
+                {
+                    src: "sourcedata",
+                    dest: "",
+                },
+            ],
+        }),
+    ],
+    build: {
+        outDir: "dist", //build folder
+    },
+    publicDir: "public",
+    base: "./", //use value from .env or '/' by default,
     css: {
         preprocessorOptions: {
             scss: {
